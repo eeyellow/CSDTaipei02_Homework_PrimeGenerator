4e07a03d86b918d0362c807c2b1a9ef67ac1c068
Ray Huang <eeyellow@gmail.com>
2016-04-17 18:50:31 +0800

    Init

---
+++ TestPrimeGenerator/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+namespace VIVALACODE
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
