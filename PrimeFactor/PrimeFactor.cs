c6350fe2f0d7a0e25657abd48212586fa15260a8
Ray Huang <eeyellow@gmail.com>
2016-04-17 20:35:03 +0800

    feat: add dependent project
    加入相關的Project

---
+++ PrimeFactor/PrimeFactor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VIVALACODE
+{
+    /// <summary>
+    /// 質因數分解
+    /// </summary>
+    public static class PrimeFactor
+    {
+        public static List<int> breakdown(int num)
+        {
+            List<int> result = new List<int>();
+
+            for (int i = 2; i <= num; i++)
+            {
+                while (num % i == 0 && num != 1)
+                {
+                    result.Add(i);
+                    num = num / i;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ TestPrimeFactor/UnitTestPrimeFactor.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace VIVALACODE
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod2()
+        {
+            CollectionAssert.AreEqual(new List<int> { 2 }, PrimeFactor.breakdown(2));
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            CollectionAssert.AreEqual(new List<int> { 3 }, PrimeFactor.breakdown(3));
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            CollectionAssert.AreEqual(new List<int> { 2, 2 }, PrimeFactor.breakdown(4));
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            CollectionAssert.AreEqual(new List<int> { 5 }, PrimeFactor.breakdown(5));
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            CollectionAssert.AreEqual(new List<int> { 2, 3 }, PrimeFactor.breakdown(6));
+        }
+
+        [TestMethod]
+        public void TestMethod7()
+        {
+            CollectionAssert.AreEqual(new List<int> { 7 }, PrimeFactor.breakdown(7));
+        }
+
+        [TestMethod]
+        public void TestMethod8()
+        {
+            CollectionAssert.AreEqual(new List<int> { 2, 2, 2 }, PrimeFactor.breakdown(8));
+        }
+
+        [TestMethod]
+        public void TestMethod9()
+        {
+            CollectionAssert.AreEqual(new List<int> { 3, 3 }, PrimeFactor.breakdown(9));
+        }
+
+        [TestMethod]
+        public void TestMethodBigNumber()
+        {
+            CollectionAssert.AreEqual(new List<int> { 2, 2, 3, 3, 5, 7, 11, 13, 23, 31 }, PrimeFactor.breakdown(2 * 2 * 3 * 3 * 5 * 7 * 11 * 13 * 23 * 31));
+        }
+    }
+}
